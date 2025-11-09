45a0b37874e4aa6e2a922275b74fc6cc466c97ae
liuchenxu-uppsala <chenxu.l@outlook.com>
2025-11-09 16:11:52 +0100

    LED驱动基于设备树

---
+++ 5-LEDDriver_dts/ledApp.c
@@ -0,0 +1,41 @@
+//
+// Created by Chenx on 2025/11/8.
+//
+#include "stdio.h"
+#include "unistd.h"
+#include "sys/types.h"
+#include "sys/stat.h"
+#include "fcntl.h"
+#include "stdlib.h"
+#include "string.h"
+#define LEDOFF 0
+#define LEDON 1
+int main(int argc, char *argv[]) {
+    int fd, retvalue;
+    char *filename;
+    unsigned char databuf[1];
+    if(argc != 3){
+        printf("Error Usage!\r\n");
+        return -1;
+    }
+    filename = argv[1];
+    fd = open(filename, O_RDWR);
+    if(fd < 0){
+        printf("file %s open failed!\r\n", argv[1]);
+        return -1;
+    }
+    databuf[0] = atoi(argv[2]);
+    retvalue = write(fd, databuf, sizeof(databuf));
+    if(retvalue < 0){
+        printf("LED Control Failed!\r\n");
+        close(fd);
+        return -1;
+    }
+    retvalue = close(fd);
+    if(retvalue < 0) {
+        printf("file %s close failed!\r\n", argv[1]);
+        return -1;
+    }
+    return 0;
+
+}
